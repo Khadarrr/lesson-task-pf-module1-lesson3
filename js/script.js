6ca9bb79e534727ffa54c302893df70343ea0475
Khadarrr <111360349+Khadarrr@users.noreply.github.com>
2023-01-06 09:47:29 +0100

    yh

---
+++ js/script.js
@@ -0,0 +1,67 @@
+//q1
+var firstNumber = 34;
+var secondNumber = 89;
+
+
+console.log("the first number is smaller than the second", firstNumber < secondNumber);
+
+//q2
+
+var age = 11;
+var minimunAge = "13";
+
+console.log(age > 13);
+
+var age = 9;
+var minimunAge = "13";
+
+if (age >= 13) {
+    console.log("test passed");
+}   else {
+    console.log("reqirements not meet");
+}
+
+//q3
+
+var income = "11.050";
+var maximumIncome = "13.075";
+
+console.log(income===maximumIncome);
+
+if (income>maximumIncome) {
+    console.log("requirements not meet")
+}   else {
+    console.log("requirements meet")
+}
+
+//q4
+
+var colour = "blue";
+
+if (colour = "orange") {
+    console.log("This colour is a bit rubbish");
+}   else {
+    console.log("magnifisent choice")
+}
+
+//q5
+
+var invoicePaid = false;
+console.log("Not paid", invoicePaid);
+
+//q6
+var selectedNumber = 11;
+var winningNumber = "15";
+
+
+if (winningNumber){
+    console.log ("winner chicken dinner!");
+}
+else {
+    console.log("You lose again","unknown");
+}
+
+//q7
+
+var dayOfTheWeek = 2
+
